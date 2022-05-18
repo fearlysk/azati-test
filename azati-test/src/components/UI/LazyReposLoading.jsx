0c1a49c71cbf7a4854e8e6e577d9ad7dd013280b
fearlysk <sqleye1@mail.ru>
2022-05-18 22:18:45 +0300

    added some sorting

---
+++ azati-test/src/components/UI/LazyReposLoading.jsx
@@ -0,0 +1,20 @@
+import "./LazyReposLoading.css";
+
+function LazyReposLoading() {
+    return (
+      <div className="wrapper">
+        <div className="ldsRoller">
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+    );
+  }
+  
+  export default LazyReposLoading;
--- azati-test/src/components/search/Search.jsx
+++ azati-test/src/components/search/Search.jsx
@@ -1,20 +1,33 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, Suspense } from "react";
 import fetchData from "../../api/fetchData/fetchData";
 import Repo from "../repo/Repo";
+import LazyReposLoading from "../UI/LazyReposLoading";
 import "./Search.css";
 
 const Search = () => {
 
     const [repos, setRepos] = useState([]);
-    const [value, setValue] = useState(null);
+    const [value, setValue] = useState("");
 
     const searchRepos = useMemo(
         () => repos.filter((repo) => repo.name.toLowerCase().includes(value.toLowerCase())),
         [value, repos]
       );
 
+    const sortRepos = (option, order) => {
+        if (order === "ASC") {
+        const filteredByAsc = repos.sort((a,b) => a[option] - b[option]).slice();
+        setRepos(filteredByAsc);
+        }
+        else if (order === "DESC") {
+        const filteredByDesc = repos.sort((a,b) => b[option] - a[option]).slice();
+        setRepos(filteredByDesc);
+        }
+    }
+
     useEffect(() => {
         fetchData().then(data => setRepos(data.items))
+        console.log(repos);
     }, [])
 
     return (
@@ -25,10 +38,22 @@ const Search = () => {
             placeholder="Search..."
             onChange={(event) => setValue(event.target.value)}
           />
+          <div>
+            <h3>Forks:</h3>
+            <button onClick={() => sortRepos("forks_count", "ASC")}>Sort by forks ASC</button>
+            <button onClick={() => sortRepos("forks_count", "DESC")}>Sort by forks DESC</button>
+          </div>
+          <div>
+            <h3>Watchers:</h3>
+            <button onClick={() => sortRepos("watchers_count", "ASC")}>Sort by watchers ASC</button>
+            <button onClick={() => sortRepos("watchers_count", "DESC")}>Sort by watchers DESC</button>
+          </div>
            {value !== "" ? (
             <div>
               {searchRepos.map(repo =>
-                <Repo key={repo.id} repo={repo}/>
+                <Suspense key={repo.id} fallback={<LazyReposLoading />}>
+                  <Repo key={repo.id} repo={repo}/>
+                </Suspense>  
               )}
             </div>
             ) : null}
